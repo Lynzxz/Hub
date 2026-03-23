6c10c6959b169d2f382a9690e70147477a6a7464
dzaky <fynrxt@gmail.com>
2026-03-24 01:01:13 +0700

    Delete api directory

--- api/sql.js
+++
@@ -1,9 +0,0 @@
-// api/sql.js
-export default function handler(req, res) {
-    res.json({
-        success: true,
-        vulnerable: true,
-        parameters: ['id', 'page'],
-        payloads: ["' OR '1'='1", "1' AND '1'='1"]
-    });
-}
